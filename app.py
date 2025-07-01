a8acc2246cb52380710a54db6e74fc4bbf43e345
nencyai <nancykushwah768@gmail.com>
2025-07-01 14:46:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,9 +41,13 @@ if uploaded_file:
     img_input = np.expand_dims(img_array, axis=0)
 
     # Predict
-    prediction = model.predict(img_input)
+    raw_prediction = model.predict(img_input)
+    prediction = tf.nn.softmax(raw_prediction)  # Normalize output
     predicted_class = np.argmax(prediction)
     confidence = np.max(prediction)
 
+   if confidence < 0.70:
+    st.error("⚠️ Uncertain result. Please upload a clearer skin image.")
+   else:
     st.markdown(f"### 🔍 Prediction: **{label_map[predicted_class]}**")
     st.markdown(f"Confidence: `{confidence:.2f}`")
